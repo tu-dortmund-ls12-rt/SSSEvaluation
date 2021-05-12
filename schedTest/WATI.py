a412ad1ab3460bf300eee3de56ba70478f06126a
HarunTeper <hteper@web.de>
2021-05-12 15:53:08 +0200

    fixed WATI-constraints

--- schedTest/WATI.py
+++ schedTest/WATI.py
@@ -20,7 +20,6 @@ def WATI(tasks):
         #Variables for loops
         len_hp_tasks = len(hp_tasks)
         len_c_segs = len(tss['Cseg'])
-        len_s_segs = len(tss['Sseg'])
 
         #Variables for Task tss and higher priority tasks
         c_segs = tss['Cseg']
@@ -54,18 +53,24 @@ def WATI(tasks):
 
         #Define variables
         Rssj = m.addMVar((len_c_segs) ,vtype=GRB.CONTINUOUS   ,name="Rssj")
-        Sssj = m.addMVar((len_s_segs) ,vtype=GRB.CONTINUOUS   ,name="Sssj")
         NikjCont = m.addMVar((len_hp_tasks,len_c_segs) ,vtype=GRB.CONTINUOUS   ,name="NikjCont")
+        NikjCeil = m.addMVar((len_hp_tasks,len_c_segs) ,vtype=GRB.INTEGER   ,name="NikjCeil")
         Nikj = m.addMVar((len_hp_tasks,len_c_segs) ,vtype=GRB.INTEGER   ,name="Nikj")
         Okj = m.addMVar((len_hp_tasks,len_c_segs) ,vtype=GRB.CONTINUOUS   ,name="Okj")
-        Jk = m.addMVar((len_hp_tasks) ,vtype=GRB.CONTINUOUS   ,name="Jk")
-        #Variables for constraints 8-10
-        # relkj = m.addMVar((len_hp_tasks,len_c_segs) ,vtype=GRB.CONTINUOUS   ,name="relkj")
-        # dpj = m.addMVar((len_hp_tasks,len_c_segs) ,vtype=GRB.CONTINUOUS   ,name="dpj")
-        # divpkj = m.addMVar((len_hp_tasks,len_hp_tasks,len_c_segs) ,vtype=GRB.CONTINUOUS   ,name="divpkj")
-        # floorpkj = m.addMVar((len_hp_tasks,len_hp_tasks,len_c_segs) ,vtype=GRB.INTEGER   ,name="floorpkj")
-        # sumpkj = m.addMVar((len_hp_tasks,len_hp_tasks,len_c_segs) ,vtype=GRB.CONTINUOUS   ,name="sumpkj")
-        # sumkj = m.addMVar((len_hp_tasks,len_c_segs) ,vtype=GRB.CONTINUOUS   ,name="sumkj")
+        relkj = m.addMVar((len_hp_tasks,len_c_segs) , vtype=GRB.CONTINUOUS   ,name="relkj")
+        dpj = m.addMVar((len_hp_tasks,len_c_segs) ,vtype=GRB.CONTINUOUS   ,name="dpj")
+        divpkj = m.addMVar((len_hp_tasks,len_hp_tasks,len_c_segs) ,vtype=GRB.CONTINUOUS   ,name="divpkj")
+        floorpkj = m.addMVar((len_hp_tasks,len_hp_tasks,len_c_segs) ,vtype=GRB.INTEGER   ,name="floorpkj")
+        sumpkj = m.addMVar((len_hp_tasks,len_hp_tasks,len_c_segs) ,vtype=GRB.CONTINUOUS   ,name="sumpkj")
+        sumkj = m.addMVar((len_hp_tasks,len_c_segs) ,vtype=GRB.CONTINUOUS   ,name="sumkj")
+
+        #Set lower bounds of variables for constraints 8-10
+        relkj.LB = -GRB.INFINITY
+        dpj.LB = -GRB.INFINITY
+        divpkj.LB = -GRB.INFINITY
+        floorpkj.LB = -GRB.INFINITY
+        sumpkj.LB = -GRB.INFINITY
+        sumkj.LB = -GRB.INFINITY
 
         #Maximize sum of Response Times
         m.setObjective(sum(Rssj), GRB.MAXIMIZE)
@@ -87,7 +92,7 @@ def WATI(tasks):
                     for j in range(len_c_segs)),name='c4')
 
         #Constraint 5
-        m.addConstrs(((Okj[k,j+1] >=  Okj[k,j] + Nikj[k,j]*Tj[j] - Rssj[j] + Sssj[j]- Jk[k])
+        m.addConstrs(((Okj[k,j+1] >=  Okj[k,j] + Nikj[k,j]*Tj[k] - (Rssj[j] + s_segs[j]) - Jk[k])
                     for k in range(len_hp_tasks)
                     for j in range(len_c_segs-1)),name='c5')
 
@@ -96,63 +101,74 @@ def WATI(tasks):
                     for k in range(len_hp_tasks)
                     for j in range(len_c_segs)),name='c6')
 
-        #Constraint 7
         m.addConstrs((NikjCont[k,j] >= 0 
                     for k in range(len_hp_tasks)
                     for j in range(len_c_segs)),name='c6cont')
 
-        #Constraint 8
-        m.addConstrs((NikjCont[k,j] <= (Rssj[j]-Okj[k,j])/Tj[k]
+        m.addConstrs((NikjCeil[k,j] >= 0 
+                    for k in range(len_hp_tasks)
+                    for j in range(len_c_segs)),name='c6ceil')
+
+        #Constraint 7
+        m.addConstrs((NikjCont[k,j] == (Rssj[j]-Okj[k,j])/Tj[k]
                     for k in range(len_hp_tasks)
                     for j in range(len_c_segs)),name='c7cont')
 
-        #Constraint 9
-        m.addConstrs((Nikj[k,j] >= NikjCont[k,j]
+        m.addConstrs((NikjCeil[k,j] >= NikjCont[k,j]
                     for k in range(len_hp_tasks)
                     for j in range(len_c_segs)),name='c7ceil1')
-
-        #Constraint 10
-        m.addConstrs((Nikj[k,j]-1 <= NikjCont[k,j]-IntFeasTol
+                    
+        m.addConstrs((NikjCeil[k,j]-1 <= NikjCont[k,j]-IntFeasTol
                     for k in range(len_hp_tasks)
                     for j in range(len_c_segs)),name='c7ceil2')
 
-        #Constraints 8-10 not implemented, as the constraints are not feasible anymore
+        m.addConstrs((Nikj[k,j] <= NikjCeil[k,j]
+                    for k in range(len_hp_tasks)
+                    for j in range(len_c_segs)),name='c7')
 
-        # m.addConstrs((divpkj[p,k,j] ==  (dpj[p,j]-relkj[k,j])/Tj[p]
-        #             for p in range(len_hp_tasks)
-        #             for k in range(len_hp_tasks)
-        #             for j in range(len_c_segs)),name='c8div')
+        # Constraint 8
+        m.addConstrs((divpkj[p,k,j] == (dpj[p,j]-relkj[k,j])/Tj[p]
+                    for p in range(len_hp_tasks)
+                    for k in range(len_hp_tasks)
+                    for j in range(len_c_segs)),name='c8div')
 
-        # m.addConstrs((floorpkj[p,k,j] <= divpkj[p,k,j]
-        #             for p in range(len_hp_tasks)
-        #             for k in range(len_hp_tasks)
-        #             for j in range(len_c_segs)),name='c8floor1')
+        m.addConstrs((floorpkj[p,k,j] <= divpkj[p,k,j]
+                    for p in range(len_hp_tasks)
+                    for k in range(len_hp_tasks)
+                    for j in range(len_c_segs)),name='c8floor1')
 
-        # m.addConstrs((floorpkj[p,k,j]+1 >= divpkj[p,k,j]+IntFeasTol
-        #             for p in range(len_hp_tasks)
-        #             for k in range(len_hp_tasks)
-        #             for j in range(len_c_segs)),name='c8floor2')
+        m.addConstrs((floorpkj[p,k,j]+1 >= divpkj[p,k,j]+IntFeasTol
+                    for p in range(len_hp_tasks)
+                    for k in range(len_hp_tasks)
+                    for j in range(len_c_segs)),name='c8floor2')
 
-        # m.addConstrs((sumpkj[p,k,j] == floorpkj[p,k,j]*Cj[p]
-        #             for p in range(len_hp_tasks)
-        #             for k in range(len_hp_tasks)
-        #             for j in range(len_c_segs)),name='c8sumparts')
+        m.addConstrs((sumpkj[p,k,j] >= floorpkj[p,k,j]*Cj[p]
+                    for p in range(len_hp_tasks)
+                    for k in range(len_hp_tasks)
+                    for j in range(len_c_segs)),name='c8sumparts1')
 
-        # m.addConstrs((sumkj[k,j] == sumpkj[:,k,j].sum()
-        #             for k in range(len_hp_tasks)
-        #             for j in range(len_c_segs)),name='c8sum')
+        m.addConstrs((sumpkj[p,k,j] >= 0
+                    for p in range(len_hp_tasks)
+                    for k in range(len_hp_tasks)
+                    for j in range(len_c_segs)),name='c8sumparts2')
 
-        # m.addConstrs((Rssj[j] >= relkj[k,j] + sumkj[k,j]
-        #             for k in range(len_hp_tasks)
-        #             for j in range(len_c_segs)),name='c8')
+        m.addConstrs((sumkj[k,j] == sumpkj[:,k,j].sum()
+                    for k in range(len_hp_tasks)
+                    for j in range(len_c_segs)),name='c8sum')
 
-        # m.addConstrs((relkj[k,j] == Okj[k,j] + (Nikj[k,j]-1) * Tj[k]
-        #             for k in range(len_hp_tasks)
-        #             for j in range(len_c_segs)),name='c9')
+        m.addConstrs((Rssj[j] >= relkj[k,j] + sumkj[k,j] + IntFeasTol
+                    for k in range(len_hp_tasks)
+                    for j in range(len_c_segs)),name='c8')
 
-        # m.addConstrs((dpj[p,j] == Okj[p,j] + Nikj[p,j] * Tj[p]
-        #             for p in range(len_hp_tasks)
-        #             for j in range(len_c_segs)),name='c10')
+        # Constraint 9
+        m.addConstrs((relkj[k,j] == Okj[k,j] + (Nikj[k,j]-1) * Tj[k]
+                    for k in range(len_hp_tasks)
+                    for j in range(len_c_segs)),name='c9')
+
+        # Constraint 10
+        m.addConstrs((dpj[p,j] == Okj[p,j] + Nikj[p,j] * Tj[p]
+                    for p in range(len_hp_tasks)
+                    for j in range(len_c_segs)),name='c10')
 
         #Update and optimize model
         m.update()
@@ -172,7 +188,7 @@ def WATI(tasks):
         else:
             # MILP is unfeasible and task set cant be scheduled
             m.computeIIS()
-            m.write("gmfpa.ilp")
+            m.write("wati.ilp")
             return False
     #Everything is schedulable
     return True
