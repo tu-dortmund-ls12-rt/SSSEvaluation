7283f4f4bbaf6eedeb00e769dc60ccf1f9fa03bc
HarunTeper <hteper@web.de>
2021-05-18 13:12:28 +0200

    small code changes

--- schedTest/EDA.py
+++ schedTest/EDA.py
@@ -1,30 +1,5 @@
 from schedTest import functions
 
-
-# def dbfEDA(t,task):
-# 	D=(task['period']-task['sslength'])/2
-# 	if t<D:
-# 		return 0
-# 	else:
-# 		return task['Cseg'][0]+task['Cseg'][1]+(t-D)*(task['Cseg'][0]+task['Cseg'][1])/task['period']
-
-	
-# def EDA(tasks):
-# 	sortedTasks=sorted(tasks,key= lambda x: functions.lm_cmp(x))
-# 	for i in range(len(sortedTasks)):
-# 		task=sortedTasks[i]
-# 		D=(task['period']-task['sslength'])/2
-# 		dbf=0
-# 		HEPTasks=sortedTasks[:i+1]
-# 		for jtask in HEPTasks:
-# 			dbf+=dbfEDA(D,jtask)
-# 		if dbf>D:
-# 			return False
-# 	return True
-
-def test():
-	return 1
-
 def dbfEDA(t,task,ssofftypes):
 	sum=0
 	for i in range(ssofftypes):
@@ -36,7 +11,6 @@ def dbfEDA(t,task,ssofftypes):
 	else:
 		return sum+(t-D)*(sum)/task['period']
 
-	
 def EDA(tasks, ssofftypes):
 	sortedTasks=sorted(tasks,key= lambda x: functions.lm_cmp(x))
 	for i in range(len(sortedTasks)):
