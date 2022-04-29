d3b8acda92c1f5abee473d2a89d17aed84289e46
Souvik Sarkar <Souvik>
2022-04-29 19:36:09 +0200

    drstask function updated

--- schedTest/drstask.py
+++ schedTest/drstask.py
@@ -6,71 +6,57 @@ import json
 import datetime
 from drs import drs
 
-
 DRS_exe=[]
 DRS_val=[]
-PSet=[]
+Task=[]
 
 
-def DRS_WCET(n, util=1):
-  
-   global DRS_exe
-
-   DRS_exe = drs(n, util)
-
-
-
-def CSet_generate(Pmin,numLog):
-    j=0
-    global PSet
-
-    for i in DRS_exe:
+def Period_generate(Pmin, numLog):                      #generating 10 tasks..
     
-       thN=j%1
+   j=0
+   
+   global DRS_exe, DRS_val, p 
+   temp = 0
+   for i in DRS_exe:
+           
+       thN=j%1 
        p=random.uniform(100*math.pow(10, thN), 100*math.pow(10, thN+1))
        pair={}
        pair['period']=p
        pair['execution']=i*p
-
-       #multiplying period with first utilization values to generate WCET
-
-       pair['deadline']=p
-       pair['utilization']=i
-       PSet.append(pair)
+       pair['suspension']=p*DRS_val[temp]
+       Task.append(pair)
+       temp = temp + 1
        j=j+1
-       
+
+   
+   return Task
 
 
-def suspend(Pmin, numLog):
-   j=0
-   global PSet
+def DRS_wcet(n, util=1):                                #generating 10 utilization values <=1
+     
+   global DRS_exe
 
-   for i in DRS_val:
-       thN=j%1
-       p=random.uniform(100*math.pow(10, thN), 100*math.pow(10, thN+1))
-       pair={}
-       pair['period']=p
-       pair['sslength']=i*p
-
-       #multiplying period with the other utilization values to generate the suspension time
-
-       PSet.append(pair)
-       j=j+1
-       
-   return PSet
-
-
-
-def taskGeneration_drs(NumberOfTasksPerSet,uTotal,u_Bound,l_Bound,minsslength,maxsslength,Pmin=100, numLog=1):
-
-   global DRS_val 
-
-   DRS_WCET(NumberOfTasksPerSet, uTotal)
-
-   DRS_val = drs(NumberOfTasksPerSet, uTotal, u_Bound, l_Bound)
+   DRS_exe = drs(n, util)
+   
+   
+     
+def DRS_sus(n, util, ubound, lbound):                             #generating other 10 utils
     
-   CSet_generate(Pmin,numLog)
+    global DRS_val
     
-   suspend(Pmin,numLog)
+    DRS_val = drs(n, util, ubound, lbound)
+    
+      
 
-   return PSet
+def taskGeneration_drs(NumberOfTasksPerSet,uTotal,u_Bound,l_Bound,minsslength,maxsslength,Pmin=100,numLog=1):
+
+   DRS_wcet(NumberOfTasksPerSet, uTotal)
+   
+   DRS_sus(NumberOfTasksPerSet,uTotal,u_Bound,l_Bound)
+
+   Period_generate(Pmin,numLog)
+   
+   print('number of tasks:', len(Task))
+   
+   return Task
