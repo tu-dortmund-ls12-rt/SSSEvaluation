0a60df30abb5da2dcad221bc0b6f2f0f956609c6
Souvik Sarkar <Souvik>
2022-05-08 19:30:06 +0200

    additional changes

--- schedTest/drstask.py
+++ schedTest/drstask.py
@@ -6,59 +6,78 @@ import json
 import datetime
 from drs import drs
 
-DRS_exe=[]
-DRS_val=[]
-Task=[]
+DRS_exe = []
+DRS_val = []
+Task = []
+
+#   DRS_exe[], DRS_val[] and Task[] are varaibles of list type
 
 
-def Period_generate(Pmin, numLog):                                                  #generating 10 tasks..
-    
-   j=0
-   
-   global DRS_exe, DRS_val, p 
-   temp = 0
-   for i in DRS_exe:
-           
-       thN=j%1 
-       p=random.uniform(100*math.pow(10, thN), 100*math.pow(10, thN+1))             #generating 10 periods
-       pair={}
-       pair['period']=p
-       pair['execution']=i*p                                                        #multiplying period with util1 values producing WCET
-       pair['suspension']=p*DRS_val[temp]                                           #multiplying period with util2 values producing Suspension period
-       Task.append(pair)                                                            #generating the Task Set {period, execution, suspension}
-       temp = temp + 1
-       j=j+1
+def Period_generate(Pmin, numLog, DRS_val, DRS_exe):
+    j = 0
+    temp = 0
+    for i in DRS_exe:
 
-   
-   return Task
+        thN = j % 1
+        p = random.uniform(100*math.pow(10, thN), 100*math.pow(10, thN+1))
+
+        # generates period values which are later multiplied with 
+        # DRS_exe util values and DRS_val util values to generate
+        # WCET and Suspension values respectively
+
+        pair = {}
+        pair['period'] = p
+        pair['execution'] = i*p
+        pair['suspension'] = p*DRS_val[temp]
+        Task.append(pair)
+        temp = temp + 1
+        j = j+1
+
+    return Task
+
+#   returns task sets with values {period, execution, suspension}
 
 
+def DRS_wcet(n, util_Exe, ubound, lbound):
 
-def DRS_wcet(n, util=1):                                                            #generating 10 utilization values <=1
-     
-   global DRS_exe
+    #   generates utilization values to calculate WCET
 
-   DRS_exe = drs(n, util)
-   
+    return drs(n, util_Exe, ubound, lbound)
 
-   
-     
-def DRS_sus(n, util, ubound, lbound):                                               #generating other 10 util values
-    
-    global DRS_val
-    
-    DRS_val = drs(n, util, ubound, lbound)
-    
-      
 
-def taskGeneration_drs(NumberOfTasksPerSet,uTotal,u_Bound,l_Bound,minsslength,maxsslength,Pmin=100,numLog=1):
+def DRS_sus(n, util_Sus, ubound, lbound):
 
-   DRS_wcet(NumberOfTasksPerSet, uTotal)
-   
-   DRS_sus(NumberOfTasksPerSet,uTotal,u_Bound,l_Bound)
+    #   generates utilization values to calculate suspension time
 
-   Period_generate(Pmin,numLog)
-   
-   print('number of tasks:', len(Task))                              
-   
-   return Task
+    return drs(n, util_Sus, ubound, lbound)
+
+
+def taskGeneration_drs(NumberOfTasksPerSet, uTotalExe,
+                       uTotalSus, u_Bound, l_Bound, Pmin=100, numLog=1):
+
+    DRS_exe = DRS_wcet(NumberOfTasksPerSet, uTotalExe, u_Bound, l_Bound)
+    # uTotalExe takes a value <=1 & l_Bound <= uTotalExe <= u_Bound
+    DRS_val = DRS_sus(NumberOfTasksPerSet, uTotalSus, u_Bound, l_Bound)
+    # uTotalSus can take value more than 1 & l_Bound <= uTotalSus <= u_Bound
+    Period_generate(Pmin, numLog, DRS_val, DRS_exe)
+
+    print('number of tasks:', len(Task))
+    return Task
+
+
+if __name__ == '__main__':
+    # DEBUG
+    # Start this script with:
+    # python3 -m schedTest.drstask
+    print('Randomly generate 10 tasks ...')
+
+    ts = taskGeneration_drs(10, 0.9, 2, [0.3]*10, [0.05]*10)
+
+    print('')
+    print(ts)
+    print('')
+    print('number of tasks:', len(ts))
+    print('the first task:', ts[1])
+    print('the last task:', ts[-1])
+
+    breakpoint()
