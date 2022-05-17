89a6a77e3e4da4ab74bc858db3592c41af9715ef
Souvik Sarkar <Souvik>
2022-05-17 22:58:02 +0200

    drstask function updated

--- schedTest/drstask.py
+++ schedTest/drstask.py
@@ -6,14 +6,9 @@ import json
 import datetime
 from drs import drs
 
-DRS_exe = []
-DRS_val = []
-Task = []
-
-#   DRS_exe[], DRS_val[] and Task[] are varaibles of list type
-
 
 def Period_generate(Pmin, numLog, val_ex, val_sus):
+    Task = []
     j = 0
     temp = 0
     for i in val_ex:
@@ -37,28 +32,28 @@ def Period_generate(Pmin, numLog, val_ex, val_sus):
 #   returns task sets with values {period, execution, suspension}
 
 
-def DRS_ex_sus(n, util_ex_Sus, ubound, lbound):
+def DRS_ex_sus(n, util_ex_Sus):
 
     #   generates the U^execution + suspension vector
 
-    return drs(n, util_ex_Sus, ubound, lbound)
+    return drs(n, util_ex_Sus)
 
 
-def DRS_ex(n, util_Ex, ubound_exe_sus, lbound):
+def DRS_ex(n, util_Ex, ubound_exe_sus):
 
     #   generates the U^execution taking U^execution + suspension vector as an upper_bound
 
-    return drs(n, util_Ex, ubound_exe_sus, lbound)
+    return drs(n, util_Ex, ubound_exe_sus)
 
 
 def taskGeneration_drs(NumberOfTasksPerSet, uTotal_Exe_Sus,
-                       uTotal_Exe, u_Bound, l_Bound, Pmin=100, numLog=1):
+                       uTotal_Exe, Pmin=100, numLog=1):
 
-    val_exe_sus = DRS_ex_sus(NumberOfTasksPerSet, uTotal_Exe_Sus, u_Bound, l_Bound)
+    val_exe_sus = DRS_ex_sus(NumberOfTasksPerSet, uTotal_Exe_Sus)
     # u_bound takes value between 0-1
     # uTotal_Exe_Sus takes value between 0-n
 
-    val_ex = DRS_ex(NumberOfTasksPerSet, uTotal_Exe, val_exe_sus, l_Bound)
+    val_ex = DRS_ex(NumberOfTasksPerSet, uTotal_Exe, val_exe_sus)
     # uTotal_Exe takes value <=1 & l_Bound <= uTotalSus <= u_Bound
     # u_bound will be the vector obtained from DRS_ex_sus
 
@@ -67,16 +62,18 @@ def taskGeneration_drs(NumberOfTasksPerSet, uTotal_Exe_Sus,
     for item1, item2 in sus_object:
         val_sus.append(item1 - item2)
 
-    Period_generate(Pmin, numLog, val_ex, val_sus)
-    print('Number of tasks:', len(Task))
-    return Task
+    Task_set = Period_generate(Pmin, numLog, val_ex, val_sus)
+
+    print('Number of tasks:', len(Task_set))
+
+    return Task_set
 
 
 if __name__ == '__main__':
     # DEBUG
     print('Randomly generate 10 tasks ...')
 
-    ts = taskGeneration_drs(10, 2, 1, [0.3] * 10, [0.05] * 10)
+    ts = taskGeneration_drs(10, 2, 1)
 
     print('')
     print(ts)
@@ -85,4 +82,4 @@ if __name__ == '__main__':
     print('the first task:', ts[1])
     print('the last task:', ts[-1])
 
-    breakpoint()  # here you can have a deeper look into the task set 'ts'. Quit with 'q'
+    breakpoint()
