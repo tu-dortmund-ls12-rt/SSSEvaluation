051d9cc0908d53aff559ab1b8b90a5beefb11b6c
Souvik Sarkar <Souvik>
2022-05-22 22:51:08 +0200

    drstask file docstring formatted

--- schedTest/drstask.py
+++ schedTest/drstask.py
@@ -7,7 +7,20 @@ import datetime
 from drs import drs
 
 
+
 def Period_generate(Pmin, numLog, val_ex, val_sus):
+    ''' Generates the Period values and returns the final Taskset
+
+    Args:
+        Pmin (int) : Takes a set value of 100
+        numLog (int) : Takes a pre-set value of 1
+        val_ex (list) : List of execution values
+        val_sus (list) : List of suspension values
+
+    Returns:
+        Task (list): Returns an appended list of Task-set in the form: {period, execution, suspension}
+
+    '''
     Task = []
     j = 0
     temp = 0
@@ -15,10 +28,6 @@ def Period_generate(Pmin, numLog, val_ex, val_sus):
 
         thN = j % numLog
         p = random.uniform(Pmin*math.pow(10, thN), Pmin*math.pow(10, thN+1))
-
-        # generates period values which are later multiplied with entries from val_ex, val_sus to 
-        # produce execution and suspension values respectively
-
         pair = {}
         pair['period'] = p
         pair['execution'] = i*p
@@ -29,34 +38,53 @@ def Period_generate(Pmin, numLog, val_ex, val_sus):
 
     return Task
 
-#   returns task sets with values {period, execution, suspension}
-
 
 def DRS_ex_sus(n, util_ex_Sus):
+    ''' Generates and returns the Utilization vector for execution and suspension time combined
 
-    #   generates the U^execution + suspension vector
+    Args:
+        n (int) : Number of tasks
+        util_ex_Sus (float) : Sum of all utilisation values(>=1) for execution and suspension
+
+    Returns:
+        drs (list): Returns a vector of n number of utilization values for (execution + suspension) time
+
+    '''
 
     return drs(n, util_ex_Sus)
 
 
 def DRS_ex(n, util_Ex, ubound_exe_sus):
+    ''' Generates and returns the Utilization vector for execution
 
-    #   generates the U^execution taking U^execution + suspension vector as an upper_bound
+    Args:
+        n (int) : Number of tasks
+        util_Ex (float) : Total utilisation value(<=1) for execution
+        ubound_exe_sus (float) : Every utilization value has its Upperbound set
+                                to the utilization values generetaed by DRS_ex_sus function
+
+    Returns:
+        drs (list): Returns a vector of n number of utilization values for exeecution time
+    '''
 
     return drs(n, util_Ex, ubound_exe_sus)
 
 
 def taskGeneration_drs(NumberOfTasksPerSet, uTotal_Exe_Sus,
                        uTotal_Exe, Pmin=100, numLog=1):
+    ''' Generates and returns the tasksets
+
+    Args:
+        NumberOfTasksPerSet (int) : Total number of tasks
+        uTotal_Exe_Sus (float) : Sum of all utilisation values(>=1) for execution and suspension
+        uTotal_Exe (float) : Total utilisation value(<=1) for execution
+
+    Returns:
+        Task_set (list): Returns the final Task-set
+    '''
 
     val_exe_sus = DRS_ex_sus(NumberOfTasksPerSet, uTotal_Exe_Sus)
-    # u_bound takes value between 0-1
-    # uTotal_Exe_Sus takes value between 0-n
-
     val_ex = DRS_ex(NumberOfTasksPerSet, uTotal_Exe, val_exe_sus)
-    # uTotal_Exe takes value <=1 & l_Bound <= uTotalSus <= u_Bound
-    # u_bound will be the vector obtained from DRS_ex_sus
-
     val_sus = []
     sus_object = zip(val_exe_sus, val_ex)
     for item1, item2 in sus_object:
