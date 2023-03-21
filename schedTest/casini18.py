4d55071d5e799698632f52c554c487138697ef9f
Mario Günzel <mario@MacBook-Air-von-Mario.local>
2023-03-21 14:52:29 +0100

    create file for analysis

---
+++ schedTest/casini18.py
@@ -0,0 +1,9 @@
+# FROM https://ieeexplore.ieee.org/abstract/document/8603232 Section IV
+# Partitioned Fixed-Priority Scheduling of Parallel Tasks Without Preemptions
+# Daniel Casini, Alessandro Biondi, Geoffrey Nelissen, and Giorgio Buttazzo
+# RTSS 2018
+#
+# Segmented self suspension for non-preemptive Fixed priority scheduling
+
+
+# TODO: Implement
